9e0628a170bd9cdd46d5d6dd802363eec7bdb8a8
yuuAn <nora@yuuan.net>
2014-08-07 12:27:03 +0900

    cake.phpでもSITESとSITE_NAMEを定義

--- core/Console/cake.php
+++ core/Console/cake.php
@@ -20,6 +20,9 @@ if (!defined('DS')) {
 	define('DS', DIRECTORY_SEPARATOR);
 }
 
+define('SITES', '');
+define('SITE_NAME', 'core');
+
 $dispatcher = 'Cake' . DS . 'Console' . DS . 'ShellDispatcher.php';
 
 if (function_exists('ini_set')) {
@@ -33,7 +36,7 @@ if (function_exists('ini_set')) {
 	if (file_exists($composerInstall . DS . $dispatcher)) {
 		$install = $composerInstall;
 	} elseif (!file_exists($install . DS . $dispatcher)) {
-		$install = $root . PATH_SEPARATOR .  $root . $ds . 'vendor' . $ds . 'cakephp' . $ds . 'cakephp' . $ds . 'lib';
+		$install = $root . PATH_SEPARATOR .  $root . DS . 'vendor' . DS . 'cakephp' . DS . 'cakephp' . DS . 'lib';
 	}
 
 	ini_set('include_path', $install . PATH_SEPARATOR . ini_get('include_path'));
